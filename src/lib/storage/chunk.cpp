936e7bfb40003395ea46e63acf12887e81f3bfbc
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2019-10-29 21:50:40 +0100

    step3: enable tests, implement chunk segment access

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -15,26 +15,31 @@
 namespace opossum {
 
 void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) {
-  // Implementation goes here
+  _columns.push_back(segment);
 }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
+  // TODO: error handling on index out of bounds case?
   // Implementation goes here
 }
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
-  // Implementation goes here
-  return nullptr;
+  // TODO: error handling on index out of bounds case?
+  return _columns[column_id];
 }
 
 uint16_t Chunk::column_count() const {
-  // Implementation goes here
-  return 0;
+  return _columns.size();
 }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
-  return 0;
+  if(_columns.empty()) {
+    // no columns -> empty
+    return 0;
+  } else {
+    // read row count from first column (each should have the same height)
+    return _columns[0].get()->size();
+  }
 }
 
 }  // namespace opossum
