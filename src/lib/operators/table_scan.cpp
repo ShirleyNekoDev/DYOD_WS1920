eba0ea73d87081a29f1cc684a42c0bd2dc5bc051
Tobias Zagorni <tobias@zagorni.eu>
2019-11-26 22:26:05 +0100

    implement do_scan in table scan operator

--- src/lib/operators/table_scan.cpp
+++ src/lib/operators/table_scan.cpp
@@ -26,22 +26,31 @@ std::shared_ptr<const Table> TableScan::_on_execute() {
   auto input = _in->get_output();
   auto result = std::make_shared<Table>();
 
-  for (uint16_t column_id = 0; column_id < result->column_count(); column_id++) {
-    result->add_column(input->column_name(ColumnID(column_id)), input->column_type(ColumnID(column_id)));
+  const uint16_t column_count = input->column_count();
+  const uint32_t chunk_size = input->max_chunk_size();
+
+  for (uint16_t column_id = 0; column_id < column_count; column_id++) {
+    result->add_column_definition(input->column_name(ColumnID(column_id)), input->column_type(ColumnID(column_id)));
   }
 
   //Assert(input->column_type(_column_id) == detail::type_strings[_search_value.type()]);
 
-  for (uint32_t chunk_index = 0; chunk_index < input->chunk_count(); chunk_index++) {
-    auto segment = input->get_chunk(ChunkID(chunk_index)).get_segment(_column_id);
-    auto pos_list = segment->get_indeces_of_value(_search_value);
-    auto result_chunk = std::make_shared<Chunk>();
+  std::vector<RowID> row_ids;
 
-    for (uint16_t column_id = 0; column_id < input->column_count(); column_id++) {
-      result_chunk->add_segment(std::make_shared<ReferenceSegment>(input, ColumnID(column_id), pos_list));
-    }
+  for (uint32_t input_chunk_index = 0; input_chunk_index < input->chunk_count(); input_chunk_index++) {
+    auto segment = input->get_chunk(ChunkID(input_chunk_index)).get_segment(_column_id);
+    segment->segment_scan(_search_value, _scan_type, [&column_count, &input, &input_chunk_index, &result, &row_ids, &chunk_size](ChunkOffset chunk_offset) {
+      if (row_ids.size() == chunk_size) {
+        Chunk new_chunk;
+        for (uint16_t column_id = 0; column_id < column_count; column_id++) {
+          new_chunk.add_segment(std::make_shared<ReferenceSegment>(input, ColumnID(column_id), row_ids));
+        }
+        result->emplace_chunk(std::move(new_chunk));
+      }
 
-    //auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment>(segment);
+      RowID new_row_id;
+      row_ids.push_back(RowID{ChunkID(input_chunk_index), chunk_offset});
+    });
   }
 
 
